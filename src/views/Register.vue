679b4b27d8b12a65a3ce1443720dd28c7496af49
Ivan <ivan@crucialcompliance.com>
2025-01-15 01:01:25 +0000

    Create Signup function

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,17 +5,17 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" @submit="register">
       <div>
         <label for="fullname" class="block text-sm/6 font-medium text-gray-900">Full Name</label>
         <div class="mt-2">
-          <input type="text" name="name" id="fullname" autocomplete="name" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="text" name="name" id="fullname" autocomplete="name" required="" v-model="user.name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
         <div class="mt-2">
-          <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="email" name="email" id="email" autocomplete="email" required="" v-model="user.email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
 
@@ -24,7 +24,15 @@
           <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
         </div>
         <div class="mt-2">
-          <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="password" name="password" id="password" autocomplete="current-password" required="" v-model="user.password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-900">Password Confirmation</label>
+        </div>
+        <div class="mt-2">
+          <input type="password" name="password_confirmation" id="password_confirmation" autocomplete="current-password_confirmation" v-model="user.password_confirmation" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
 
@@ -40,5 +48,28 @@
     </p>
   </div>
 </template>
-<script lang="ts">
+<script setup>
+  import store from "../store/index.js";
+  import {useRouter} from "vue-router";
+
+  const router = useRouter();
+  const user ={
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  };
+
+  function register(ev){
+    ev.preventDefault();
+    store
+      .dispatch('register', user)
+      .then((res) => {
+        router.push({
+          name: 'Dashboard'
+        })
+      })
+  }
+
+
 </script>
