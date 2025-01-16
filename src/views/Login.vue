5858795244c14b333db85bd27c025098988a4dbd
Ivan <ivan@crucialcompliance.com>
2025-01-16 20:04:21 +0000

    Create Login function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,11 +5,20 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" @submit="login">
+
+      <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+        {{errorMsg}}
+        <span @click="errorMsg = ''" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </span>
+      </div>
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
         <div class="mt-2">
-          <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="email" name="email" id="email" autocomplete="email" v-model="user.email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
 
@@ -18,7 +27,7 @@
           <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
         </div>
         <div class="mt-2">
-          <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input type="password" name="password" id="password" autocomplete="current-password" v-model="user.password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
       </div>
 
@@ -34,5 +43,30 @@
     </p>
   </div>
 </template>
-<script lang="ts">
+<script setup>
+import store from "../store/index.js";
+import {useRouter} from "vue-router";
+import {ref} from "vue";
+
+const router = useRouter();
+const user ={
+  email: '',
+  password: ''
+};
+
+let errorMsg = ref('');
+
+function login(ev){
+  ev.preventDefault();
+  store
+    .dispatch('login', user)
+    .then((res) => {
+      router.push({
+        name: 'Dashboard'
+      })
+    })
+    .catch(err => {
+      errorMsg.value = err.response.data.error;
+    })
+}
 </script>
