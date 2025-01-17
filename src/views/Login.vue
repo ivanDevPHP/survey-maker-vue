f0ee6d43b43228a3dc98a631a0979544bbce1e73
Ivan <ivanfslima@gmail.com>
2025-01-17 01:16:08 +0000

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,7 +16,7 @@
         </span>
       </div>
       <div>
-        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email Address</label>
         <div class="mt-2">
           <input type="email" name="email" id="email" autocomplete="email" v-model="user.email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
