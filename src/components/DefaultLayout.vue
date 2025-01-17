f61af5382d258fa5eceed395e81c93c4e5623963
Ivan <ivan@crucialcompliance.com>
2025-01-17 00:52:30 +0000

    Create Logout

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -136,10 +136,12 @@ export default {
     const router = useRouter();
 
     function logout(){
-      store.commit("logout")
-      router.push({
-        name: "Login",
-      });
+      store.dispatch("logout")
+        .then(() => {
+          router.push({
+            name: "Login",
+          });
+        });
     }
 
     return {
