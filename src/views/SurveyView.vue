467b882cc226a62453fc31f141a80aa201ec6dd5
Ivan <ivanfslima@gmail.com>
2025-01-22 01:39:08 +0000

    Add Image to Survey

--- src/views/SurveyView.vue
+++ src/views/SurveyView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { v4 as uuidv4 } from "uuid";
 import store from "../store/index.js";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import PageComponent from "../components/PageComponent.vue";
@@ -14,15 +14,23 @@ let model = ref({
   title: "",
   status: false,
   description: null,
-  image: null,
+  image_url: null,
   expire_date: null,
   questions: [],
 });
 
+watch(
+  () => store.state.currentSurvey.data,
+  (newVal, oldVal) => {
+    model.value = {
+      ...JSON.parse(JSON.stringify(newVal)),
+      status: newVal.status !== "draft",
+    }
+  }
+)
+
 if (route.params.id) {
-  model.value = store.state.surveys.find(
-    (s) => s.id === parseInt(route.params.id)
-  );
+  store.dispatch('getSurvey', route.params.id);
 }
 
 function onImageChoose(ev){
