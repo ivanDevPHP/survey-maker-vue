0bf582662c42b5af90d7c696199d77bea45b369a
Ivan <ivanfslima@gmail.com>
2025-02-05 20:00:48 +0000

    Implement Pagination

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,7 @@ const store = createStore({
     },
     surveys: {
       loading: false,
+      links: [],
       data: []
     },
     questionTypes: ["text", "select", "radio", "checkbox", "textarea",],
@@ -61,9 +62,10 @@ const store = createStore({
     deleteSurvey({}, id){
       return axiosClient.delete(`/survey/${id}`)
     },
-    getSurveys({commit}, id){
+    getSurveys({commit}, {url = null} = {}){
+      url = url || '/survey'
       commit('setSurveysLoading', true);
-      return axiosClient.get("/survey").then((res) =>{
+      return axiosClient.get(url).then((res) =>{
         commit('setSurveysLoading', false)
         commit("setSurveys", res.data);
         return res;
@@ -102,6 +104,7 @@ const store = createStore({
       state.surveys.loading = loading;
     },
     setSurveys: (state, surveys) => {
+      state.surveys.links = surveys.meta.links;
       state.surveys.data = surveys.data;
     },
     logout: (state) => {
