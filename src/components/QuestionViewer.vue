084d831ac2454ab4f0e7c8c44eb6ca90dd2fc8f3
Ivan <ivanfslima@gmail.com>
2025-02-19 20:52:19 +0000

    Display Questions on public page

--- src/components/QuestionViewer.vue
+++ src/components/QuestionViewer.vue
@@ -1,8 +1,103 @@
 <script setup>
+import { ref } from "vue";
+const { question, index, modelValue } = defineProps({
+  question: Object,
+  index: Number,
+  modelValue: [String, Array],
+});
+const emits = defineEmits(["update:modelValue"]);
+
+let model;
+if (question.type === "checkbox"){
+  model = ref({});
+}
 </script>
 
 <template>
-  TEST VIEW
+  <fieldset class="mb-4">
+    <div>
+      <legend class="text-base font-medium text-gray-900">
+        {{ index + 1 }}. {{ question.question }}
+      </legend>
+      <p class="text-gray-500 text-sm">
+        {{ question.description }}
+      </p>
+    </div>
+    <div class="mt-3">
+      <div v-if="question.type === 'select'">
+        <select
+          :value="modelValue"
+          @change="emits('update:modelValue', $event.target.value)"
+          class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white
+          rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+          sm:text-sm">
+            <option value="">Please Select</option>
+            <option v-for="option in question.data.options" :key="option.uuid" :value="option.text">
+              {{ option.text }}
+            </option>
+        </select>
+      </div>
+      <div v-else-if="question.type === 'radio'">
+        <div
+          v-for="(option, ind) of question.data.options"
+          :key="option.uuid"
+          class="flex items-center"
+        >
+          <input
+            :id="option.uuid"
+            :name="'question' + question.id"
+            :value="option.text"
+            @change="emits('update:modelValue', $event.target.value)"
+            type="radio"
+            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+          />
+          <label
+            :for="option.uuid"
+            class="ml-3 block text-sm font-medium text-gray-700"
+          >
+            {{ option.text }}
+          </label>
+        </div>
+      </div>
+      <div v-else-if="question.type === 'checkbox'">
+        <div
+          v-for="(option, ind) of question.data.options"
+          :key="option.uuid"
+          class="flex items-center"
+        >
+          <input
+            :id="option.uuid"
+            v-model="model[option.text]"
+            @change="onCheckboxChange"
+            type="checkbox"
+            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+          />
+          <label
+            :for="option.uuid"
+            class="ml-3 block text-sm font-medium text-gray-700"
+          >
+            {{ option.text }}
+          </label>
+        </div>
+      </div>
+      <div v-else-if="question.type === 'text'">
+        <input
+          type="text"
+          :value="modelValue"
+          @input="emits('update:modelValue', $event.target.value)"
+          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        />
+      </div>
+      <div v-else-if="question.type === 'textarea'">
+        <textarea
+          :value="modelValue"
+          @input="emits('update:modelValue', $event.target.value)"
+          rows="3"
+          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        ></textarea>
+      </div>
+    </div>
+  </fieldset>
 </template>
 
 <style>
