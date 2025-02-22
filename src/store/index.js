320aa7ade58172c7172ad2f0af3c28782d1f692b
Ivan <ivanfslima@gmail.com>
2025-02-22 16:31:23 +0000

    Implement Dashboard

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,10 @@ const store = createStore({
       data: {},
       token: sessionStorage.getItem("TOKEN"),
     },
+    dashboard: {
+      loading: false,
+      data: {}
+    },
     currentSurvey: {
       loading: false,
       data: {}
@@ -26,6 +30,19 @@ const store = createStore({
   },
   getters: {},
   actions: {
+    getDashboardData({commit}){
+      commit('dashboardLoading', true)
+      return axiosClient.get(`/dashboard`)
+        .then((res) => {
+          commit('dashboardLoading', false)
+          commit('setDashboardData', res.data)
+          return res;
+        })
+        .catch(error => {
+          commit('dashboardLoading', false)
+          return error;
+        })
+    },
     getSurvey({commit}, id){
       commit("setCurrentSurveyLoading", true);
       return axiosClient
@@ -110,6 +127,12 @@ const store = createStore({
     }
   },
   mutations: {
+    dashboardLoading: (state, loading) => {
+      state.dashboard.loading = loading;
+    },
+    setDashboardData: (state, data) => {
+      state.dashboard.data = data;
+    },
     setCurrentSurveyLoading: (state, loading) => {
       state.currentSurvey.loading = loading;
     },
