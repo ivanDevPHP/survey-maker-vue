362ae2d8e180cd415a3f441fe8b52df7eeeeb331
Ivan <ivanfslima@gmail.com>
2025-03-06 20:10:02 +0000

    feat: show message when no surveys are available

---
+++ src/components/NoSurvey.vue
@@ -0,0 +1,19 @@
+<script>
+import {useRouter} from "vue-router";
+const router = useRouter();
+</script>
+
+<template>
+  <div class="flex flex-col items-center text-center">
+    <p>You don't have any surveys yet.</p>
+    <router-link
+      :to="{ name: 'SurveyCreate' }"
+      class="text-blue-500 hover:underline mt-2">
+      Click here to create your first one.
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
